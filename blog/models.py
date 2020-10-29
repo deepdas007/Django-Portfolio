4ac1b978f66aa8a25826bc4fb14fa5758f95bdc8
deep0207 <57975801+deep0207@users.noreply.github.com>
2020-10-29 17:06:27 +0530

    Added a new jobs page in the portfolio

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Blogs(models.Model):
+    blog_title = models.CharField('Title', max_length=20)
+    pub_date = models.DateField('Publish Date')
+    content = models.TextField('Body')
+    head_image = models.ImageField(upload_to='images/')
