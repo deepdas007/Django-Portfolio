7d50e488bdb8483844972a2ea83de2f3afc0b839
deep0207 <57975801+deep0207@users.noreply.github.com>
2020-11-02 14:36:04 +0530

    Completed the portfolio project

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,16 @@ from django.db import models
 
 
 class Blogs(models.Model):
-    blog_title = models.CharField('Title', max_length=20)
-    pub_date = models.DateField('Publish Date')
+    blog_title = models.CharField('Title', max_length=200)
+    pub_date = models.DateTimeField('Publish Date & Time')
     content = models.TextField('Body')
     head_image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.blog_title
+
+    def summary(self):
+        return self.content[:100]
+
+    def pub_date_new(self):
+        return self.pub_date.strftime('%b %e %Y')
